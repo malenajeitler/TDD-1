6694353bf53a525c23412a4b8406f9316e8cb05b
malenajeitler <malenajeitler@gmail.com>
2026-05-06 11:43:17 +0200

    added TC_2

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,7 +3,13 @@ describe('String Calculator', function() {
 		calculator = new StringCalculator();
 	});
 
+	//TC_1
 	it('should return 0 for an empty string', function() {
 		expect(calculator.add('')).toEqual(0);
+	});
+
+	// TC_2
+	it('should return 1 for a string of "1"', function() {
+		expect(calculator.add('1')).toEqual(1);
 	});
 });
