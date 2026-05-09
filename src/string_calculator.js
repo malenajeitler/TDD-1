330c9d7593723f1d65739fd2ea03e90bb7803021
malenajeitler <malenajeitler@gmail.com>
2026-05-09 13:24:55 +0200

    implemented code for TC_5, no refactoring needed

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -2,7 +2,12 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	if (string_numbers.includes(',')) {
-		result = parseInt(string_numbers.split(',')[0]) + parseInt(string_numbers.split(',')[1]);
+		if (string_numbers.length > 5) {
+			result = parseInt(string_numbers.split(',')[0]) + parseInt(string_numbers.split(',')[1]) + parseInt(string_numbers.split(',')[2]);
+		}
+		else {
+			result = parseInt(string_numbers.split(',')[0]) + parseInt(string_numbers.split(',')[1]);
+		}
 	} else if (string_numbers.length > 0) {
 		result = parseInt(string_numbers);
 	} else if (string_numbers.length == 0) {
