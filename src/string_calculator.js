00c357d233d8ba5cdda3d30a900a36d6685ece5d
malenajeitler <malenajeitler@gmail.com>
2026-05-09 12:49:47 +0200

    refactoring done, all test cases OK

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -3,9 +3,10 @@ function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	if (string_numbers.includes(',')) {
 		result = parseInt(string_numbers.split(',')[0]) + parseInt(string_numbers.split(',')[1]);
-	}
-	 else if (string_numbers.length > 0) {
+	} else if (string_numbers.length > 0) {
 		result = parseInt(string_numbers);
+	} else if (string_numbers.length == 0) {
+		return 0;
 	}
 	return result;
 };
