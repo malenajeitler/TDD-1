8833bbc1b514b1e8b5933c2d6fdece015cc4a383
malenajeitler <malenajeitler@gmail.com>
2026-05-09 12:37:21 +0200

    added TC_4

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -8,13 +8,18 @@ describe('String Calculator', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
 
-	// TC_2
+	//TC_2
 	it('should return 1 for a string of "1"', function() {
 		expect(calculator.add('1')).toEqual(1);
 	});
 
-	// TC_3
+	//TC_3
 	it('should return 3 for a string of "1,2"', function() {
 		expect(calculator.add('1,2')).toEqual(3);
 	});
+
+	//TC_4
+	it('should return 28 for a string of "1,27"', function() {
+		expect(calculator.add('1,27')).toEqual(28);
+	})
 });
